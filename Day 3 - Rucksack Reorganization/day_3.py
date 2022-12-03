070a5f716b236767ecee318f02b19722e326d846
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-12-04 00:13:00 +0700

    chore: day 3

--- Day 3 - Rucksack Reorganization/day_3.py
+++ Day 3 - Rucksack Reorganization/day_3.py
@@ -4,27 +4,26 @@ characters = ascii_lowercase + ascii_uppercase
 
 with open("input.txt") as f:
   data = f.read().splitlines()
-  
-  
+
+
 def find_sum_index(data):
   data = [characters.index(j) + 1 for i in data for j in i]
   return sum(data)
 
+
 def part_1(data):
   middle = [len(line) // 2 for line in data]
-  data = [[set(line[:middle[i]]), set(line[middle[i]:])]
-          for i, line in enumerate(data)]
-  data = [i[0].intersection(i[1]) for i in data]
-  sum_index = find_sum_index(data)
+  sum_index = find_sum_index([set(line[:middle[i]]).intersection(set(line[middle[i]:]))
+                              for i, line in enumerate(data)])
   print(sum_index)
 
 
 def part_2(data):
   data = [data[i:i+3] for i in range(0, len(data), 3)]
-  data = [[set(i[0]),set(i[1]),set(i[2])] for i in data]
-  data = [i[0].intersection(i[1],i[2]) for i in data]
-  sum_index = find_sum_index(data)
+  sum_index = find_sum_index(
+      [set(i[0]).intersection(set(i[1]), set(i[2])) for i in data])
   print(sum_index)
+
 
 part_1(data)
 part_2(data)
