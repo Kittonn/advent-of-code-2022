c86b888f19f6040e89e6c95a466fb81b5f873197
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-12-04 18:58:34 +0700

    chore: day 4 (part 2)

--- Day 4 - Camp Cleanup/day_4.py
+++ Day 4 - Camp Cleanup/day_4.py
@@ -9,9 +9,7 @@ data = [[[int(j[0]), int(j[1])] for j in i] for i in data]
 def part_1(data):
   count = 0
   for i in data:
-    if i[0][0] >= i[1][0] and i[0][1] <= i[1][1]:
-      count += 1
-    elif i[1][0] >= i[0][0] and i[1][1] <= i[0][1]:
+    if i[0][0] >= i[1][0] and i[0][1] <= i[1][1] or i[1][0] >= i[0][0] and i[1][1] <= i[0][1]:
       count += 1
   print(count)
 
@@ -19,17 +17,10 @@ def part_1(data):
 def part_2(data):
   overlap = 0
   for i in data:
-    if i[0][0] < i[1][0] and i[0][1] < i[1][0]:
+    if not(i[0][0] < i[1][0] and i[0][1] < i[1][0] or i[0][0] > i[1][1] and i[0][1] > i[1][1]):
       overlap += 1
-    elif i[1][0] < i[0][0] and i[1][1] < i[0][0]:
-      overlap += 1
-    elif i[0][0] > i[1][1] and i[0][1] > i[1][1]:
-      overlap += 1
-    elif i[1][0] > i[0][1] and i[1][1] > i[0][1]:
-      overlap += 1
-    
-  print(len(data) - overlap)
-  
+
+  print(overlap)
 
 
 part_1(data)
