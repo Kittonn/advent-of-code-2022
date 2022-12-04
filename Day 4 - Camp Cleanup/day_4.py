387e687c45dec1e5d584431df48396ec96089a8a
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-12-04 14:58:23 +0700

    feat: day 4 (part 1)

---
+++ Day 4 - Camp Cleanup/day_4.py
@@ -0,0 +1,29 @@
+with open("input.txt") as f:
+  data = [line.split(",") for line in f.read().splitlines()]
+
+
+data = [[j.split("-") for j in i] for i in data]
+data = [[[int(j[0]), int(j[1])] for j in i] for i in data]
+
+
+def part_1(data):
+  count = 0
+  for i in data:
+    if i[0][0] >= i[1][0] and i[0][1] <= i[1][1]:
+      count += 1
+    elif i[1][0] >= i[0][0] and i[1][1] <= i[0][1]:
+      count += 1
+  print(count)
+
+
+def part_2(data):
+  overlap = 0
+  for i in data:
+    pass
+    
+  print(overlap)
+  
+
+
+part_1(data)
+part_2(data)
